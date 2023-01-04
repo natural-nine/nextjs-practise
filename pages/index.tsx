c4fb30755235eb23715f18143fe029adb03c89fa
natural-nine <wan.jun.bruce@gmail.com>
2023-01-04 21:16:48 +0900

    initial-settings

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+
+import styled from "styled-components";
+
+export default function Home() {
+  return <Wrap>hello</Wrap>;
+}
+
+const Wrap = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
---
+++ styles/global-styles.ts
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+ 
+  * { font-family: 'Spoqa Han Sans', 'Spoqa Han Sans JP', 'Sans-serif';}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-size: 100%;
+    font: inherit;
+	  vertical-align: baseline;
+	font: inherit;
+	vertical-align: baseline;
+  }
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+  body {
+  box-sizing: border-box;  
+  }
+  a {
+    color: #000;
+    text-decoration: none;
+  }
+  html{
+    width: 100%;
+    height: 100%;
+    font-size: 62.5%;
+  }
+`;
