e2bc02824413b367c1ec78fecd6803348a48b9e5
natural-nine <wan.jun.bruce@gmail.com>
2023-01-05 16:18:01 +0900

    Feat: 헤더 완성

---
+++ components/layout/Toggle.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import styled from "styled-components";
+import classes from "./toggle.module.css";
+
+const Toggle = () => {
+  const [toggle, setToggle] = useState(false);
+  const toggleChange = () => {
+    setToggle((toggle: boolean) => !toggle);
+  };
+  console.log(toggle);
+  return (
+    <Wrap>
+      <span>Theme Mode :</span>
+      <label className={classes.switch}>
+        <input type="checkbox" checked={toggle} onChange={toggleChange} />
+        <span className={classes.slider}></span>
+      </label>
+      <span>{toggle ? "Light" : "Dark"}</span>
+    </Wrap>
+  );
+};
+
+const Wrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  span {
+    margin-left: 10px;
+  }
+`;
+
+export default Toggle;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,26 @@
-
+import axios from "axios";
 import styled from "styled-components";
 
-export default function Home() {
-  return <Wrap>hello</Wrap>;
+export default function Home(props: any) {
+  // console.log(props.data.slice(0, 100));
+  return <Wrap>ggggg</Wrap>;
 }
 
 const Wrap = styled.div`
   width: 100%;
   height: 100vh;
 `;
+
+// export const getStaticProps = async () => {
+//   try {
+//     const response = await axios.get("https://restcountries.com/v2/all");
+//     const { data }: any = response;
+//     return {
+//       props: {
+//         data,
+//       },
+//     };
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
